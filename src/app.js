aed25298cbeb13e97d60348b7ea45a14322884bc
Mustafa Khamisi <mustafangure95@gmail.com>
2019-04-18 15:47:49 +0300

    feat: Create a custom router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,32 @@
 $ = jQuery = require("jquery");
+var React = require('react');
+var Home = require('./components/homePage');
+var AboutPage = require('./components/about/aboutPage');
 
-var app = console.log("Hello, this is react!");
-
-module.exports = app;
+(function (win) {
+  'use strict';
+  var App = React.createClass({
+    render: function() {
+      var Child;
+      switch (this.props.route) {
+        case 'about':
+          Child = AboutPage;
+          break;
+        default:
+          Child = Home;
+          break;
+      }
+      return (
+        <div>
+          <Child/>
+        </div>
+      );
+    }
+  });
+  function render(){
+    var route = win.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+  }
+  window.addEventListener('hashchange', render);
+  render();
+})(window);
