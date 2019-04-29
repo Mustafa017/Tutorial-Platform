275fd6b37e7fb417a1a0d09a8d0f13f53a16f25f
Mustafa Khamisi <mustafangure95@gmail.com>
2019-04-29 15:10:35 +0300

    feat: Change anchor tags to Links to enable easy reference to pages

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,18 +1,19 @@
 'use strict';
 var React = require('react');
+var Link = require('react-router').Link;
 
 var header = React.createClass({
   render: function() {
     return (
       <nav className="navbar navbar-default">
         <div className="container-fluid">
-          <a href="/" className="navbar-brand">
+          <Link to="app" className="navbar-brand">
             <img src="" alt="site logo"/>
-          </a>
+          </Link>
           <ul className="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/#authors">Authors</a></li>
-            <li><a href="/#about">About</a></li>
+            <li><Link to="app">Home</Link></li>
+            <li><Link to="authors">Authors</Link></li>
+            <li><Link to="about">About</Link></li>
           </ul>
         </div>
       </nav>
