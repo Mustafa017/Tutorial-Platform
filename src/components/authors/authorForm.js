55c57c2313f032b710faff7754e0be6dadb99e04
Mustafa Khamisi <mustafangure95@gmail.com>
2019-05-01 08:48:59 +0300

    feat: Use reusable text input function

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -1,33 +1,35 @@
 'use strict';
 var React = require('react');
+var TextInput = require('../common/textInput');
 
 var authorForm = React.createClass({
+  propTypes: {
+    author: React.PropTypes.object.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onError: React.PropTypes.object
+  },
+
   render: function() {
     return (
       <div>
         <h1>Manage Author</h1>
         <form>
-          <label htmlFor="firstName">First Name</label>
-          <input type="text"
-                className="form-control"
-                name="firstName"
-                ref="firstName"
-                placeholder="First Name"
-                onChange={this.props.onChange}
-                value={this.props.author.firstName} />
-          <br/>
+          <TextInput 
+            name="firstName" 
+            label="First Name" 
+            onChange={this.props.onChange}
+            value={this.props.author.firstName}
+            error={this.props.onError.firstName} />
 
-          <label htmlFor="lastName">Last Name</label>
-          <input type="text"
-                className="form-control"
-                name="lastName"
-                ref="lastName"
-                placeholder="Last Name"
-                onChange={this.props.onchange}
-                value={this.props.author.lastName} />
-          <br/>
-          
-          <input type="submit" value="Save" className="btn btn-default"/>
+          <TextInput 
+            name="lastName" 
+            label="Last Name" 
+            onChange={this.props.onChange}
+            value={this.props.author.lastName} 
+            error={this.props.onError.lastName} />
+
+          <input type="submit" value="Save" className="btn btn-default" onClick={this.props.onSave} />
         </form>
       </div>
     );
