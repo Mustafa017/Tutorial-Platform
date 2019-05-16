5cec85685b372ca634e50923165be8003e01691e
Mustafa Khamisi <mustafangure95@gmail.com>
2019-05-16 10:12:49 +0300

    feat: Add onClick handler to delete author

--- src/components/authors/authorList.js
+++ src/components/authors/authorList.js
@@ -1,16 +1,25 @@
 'use strict';
 var React = require('react');
 var Link = require('react-router').Link;
+var toastr = require('toastr');
+var AuthorAction = require('../../actions/authorAction');
 
 var authorList = React.createClass({
   propTypes: {
     authors: React.PropTypes.array.isRequired
   },
   
+  deleteAuthor: function(id, event) {
+    event.preventDefault();
+    AuthorAction.deleteAuthor(id);
+    toastr.success("Author Deleted!");
+  },
+
   render: function() {
     var createAuthorRow = function(author) {
       return (
         <tr key={author.id}>
+          <td><a href={"/author/" + author.id} onClick={this.deleteAuthor.bind(this, author.id)}>delete</a></td>
           <td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
           <td>{author.firstName} {author.lastName}</td>
         </tr>
@@ -20,6 +29,7 @@ var authorList = React.createClass({
       <div>
         <table className="table">
           <thead>
+            <th>Action</th>
             <th>ID</th>
             <th>Name</th>
           </thead>
