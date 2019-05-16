bac6def3c27fe400e4c559fb59253615904a084b
Mustafa Khamisi <mustafangure95@gmail.com>
2019-05-16 10:15:20 +0300

    feat: Add action types to update and delete author

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -4,7 +4,8 @@ var keyMirror = require('react/lib/keyMirror');
 var actionTypes = keyMirror({
   INITIALIZE: null,
   CREATE_AUTHOR: null,
-  UPDATE_AUTHOR: null
+  UPDATE_AUTHOR: null,
+  DELETE_AUTHOR: null
 });
 
 module.exports = actionTypes;
